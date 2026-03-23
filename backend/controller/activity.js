13d86802e64a4b9199508a7c065762e3a72d449b
Ella R <ellaraputri@gmail.com>
2026-03-23 23:48:47 +0700

    feat: get activity list api

---
+++ backend/controller/activity.js
@@ -0,0 +1,32 @@
+import { db } from "../config/firestore.js";
+import { error400, error500 } from "../config/error.js";
+
+
+export const getList = async (req, res) => {
+  try {
+    const snapshot = await db.collection("activities").get();
+    if (snapshot.empty) {
+      return res.status(200).json({
+        activities: [],
+        success: true,
+        message: "No activities found",
+      });
+    }
+
+    const activities = snapshot.docs.map(doc => ({
+      id: doc.id,
+      name: doc.data().name,
+      type: doc.data().type,
+    }));
+
+    return res.status(200).json({
+      activities,
+      success: true,
+      message: "Activities fetched successfully",
+    });
+
+  } catch (error) {
+    console.error(error);
+    return error500(res);
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { db } from './config/firestore.js';
 import studentRouter from './router/student.js';
 import teamRouter from './router/team.js';
 import resultRouter from './router/result.js';
+import activityRouter from './router/activity.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -18,6 +19,7 @@ app.get('/', (req, res) => res.send("Firebase API working"));
 app.use("/api/auth", studentRouter);
 app.use("/api/team", teamRouter);
 app.use("/api/result", resultRouter);
+app.use("/api/activity", activityRouter);
 
 // Start server
 app.listen(PORT, "0.0.0.0", () => {
