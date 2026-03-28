6130801462caac9c90dfbb56ac2e2b6f2aa506c4
ellisraputri <ellisraputri@gmail.com>
2026-03-28 13:29:02 +0700

    feat: add sqlite

--- frontend/context/AppContext.tsx
+++ frontend/context/AppContext.tsx
@@ -1,6 +1,7 @@
+import { getUser, initDB, saveTeam, saveUser } from "@/services/sqlite";
 import { StudentDetail } from "@/services/student/student.type";
 import { TeamDetail } from "@/services/team/team.type";
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import React, { createContext, useContext, useState, ReactNode, useEffect } from "react";
 
 type AppContextType = {
   user: StudentDetail | null;
@@ -14,6 +15,20 @@ const AppContext = createContext<AppContextType | undefined>(undefined);
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<StudentDetail | null>(null);
   const [team, setTeam] = useState<TeamDetail | null>(null);
+
+  useEffect(() => {
+    initDB();
+  }, []);
+
+  const setUserState = (newUser: StudentDetail | null) => {
+    setUser(newUser);
+    saveUser(newUser);
+  };
+
+  const setTeamState = (newTeam: TeamDetail |null) => {
+    setTeam(newTeam);
+    saveTeam(newTeam);
+  }
 
   return (
     <AppContext.Provider value={{ user, setUser, team, setTeam }}>
---
+++ frontend/services/sqlite.ts
@@ -0,0 +1,63 @@
+import * as SQLite from "expo-sqlite";
+import {StudentDetail} from "./student/student.type"
+import { TeamDetail } from "./team/team.type";
+
+const db = SQLite.openDatabaseAsync("app.db");
+
+export const initDB = async () => {
+  const database = await db;
+  await database.execAsync(
+    `CREATE TABLE IF NOT EXISTS user (
+      id TEXT PRIMARY KEY,
+      data TEXT
+    );
+    CREATE TABLE IF NOT EXISTS team (
+      id TEXT PRIMARY KEY,
+      data TEXT
+    );`
+  );
+};
+
+export const saveUser = async (user: StudentDetail | null) => {
+  const database = await db;
+  await database.runAsync(
+    "INSERT OR REPLACE INTO user (id, data) VALUES (?, ?)",
+    ["current", JSON.stringify(user)]
+  );
+};
+
+export const getUser = async (): Promise<StudentDetail | null> => {
+  const database = await db;
+  const result = await database.getFirstAsync<{ data: string }>(
+    "SELECT * FROM user WHERE id = ?",
+    ["current"]
+  );
+  if (result) {
+    return JSON.parse(result.data);
+  } else {
+    return null;
+  }
+};
+
+export const saveTeam = async (team: TeamDetail | null) => {
+  const database = await db;
+  await database.runAsync(
+    "INSERT OR REPLACE INTO team (id, data) VALUES (?, ?)",
+    ["current", JSON.stringify(team)]
+  );
+};
+
+export const getTeam = async (): Promise<TeamDetail | null> => {
+  const database = await db;
+  const result = await database.getFirstAsync<{ data: string }>(
+    "SELECT * FROM team WHERE id = ?",
+    ["current"]
+  );
+  if (result) {
+    return JSON.parse(result.data);
+  } else {
+    return null;
+  }
+};
+
+
